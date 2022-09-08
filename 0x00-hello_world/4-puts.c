fbddad349b7300c3b7b35467c2e4afd49c9d722b
Zoolian <gamebwoy13@gmail.com>
2022-09-08 22:48:15 +0000

    pushing my project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print string to the consol
+ * Return: return 0 when successed
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+0- is ued to run a file throug the preprocessor and store the result into another file called c
