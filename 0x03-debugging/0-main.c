3882651485638197198c68c932f55c589dee846d
Zoolian <gamebwoy13@gmail.com>
2022-09-16 01:13:56 +0000

    my files 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - test fuction that print
+ * 0 if number is zero
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
