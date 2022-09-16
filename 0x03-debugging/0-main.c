017a8b6c0a6b25ebde0f3ec0db99652f75480671
Zoolian <gamebwoy13@gmail.com>
2022-09-16 01:31:38 +0000

    adding debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * main - test fuction that print
- * 0 if number is zero
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
+return (0);
 }
