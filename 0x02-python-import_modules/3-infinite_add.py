70887428ec0272244ee857df97ef4f704dea974e
abdulbasitosman <www.osmanabdulbasit@gmail.com>
2022-12-03 02:15:26 -0800

    adding

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     result = 0
     size = len(sys.argv)
     for i in range(1, size):
-        result += int(sys.argv[i]
+        result += int(sys.argv[i])
     print("{}".format(result))
