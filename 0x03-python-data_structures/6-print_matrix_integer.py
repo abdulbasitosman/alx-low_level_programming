aa077a7fc8d534fb338a5e4fc0c497407052eb46
abdulbasitosman <www.osmanabdulbasit@gmail.com>
2022-12-07 04:45:09 -0800

    commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for item in matrix:
-        for sub_item in item:
-            print('{:d}'.format(sub_item), end="" if sub_item != item[-1] else '')
+    for i in matrix:
+        for j in i:
+            print('{:d}'.format(j), end='' if j != i[-1] else '')
         print()
